9869484ae9080fdc3d4b994ddd4fbc03be788e05
LaCometeCookie <come.boutevin@gmail.com>
2023-11-21 16:13:09 +0100

    Ajout de la

---
+++ chargement_donnees.c
@@ -0,0 +1,13 @@
+#include "header.h"
+
+void chargementGrapheOriente() {
+    FILE* precedences = fopen("precedences.txt", "r");
+    FILE* durees = fopen("operations.txt", "r");
+
+    if (precedences == NULL || durees == NULL) {
+        printf("Erreur lors de l'ouverture des fichiers.\n");
+        exit(1);
+    }
+
+
+}
--- header.h
+++ header.h
@@ -1,16 +1,68 @@
-//
-// Created by kimih on 20/11/2023.
-//
-
 #ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_6_97_GIT_HEADER_H
 #define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_6_97_GIT_HEADER_H
 
+
+/// ██████╗ ██╗██████╗ ██╗     ██╗ ██████╗ ████████╗██╗  ██╗███████╗ ██████╗ ██╗   ██╗███████╗███████╗
+/// ██╔══██╗██║██╔══██╗██║     ██║██╔═══██╗╚══██╔══╝██║  ██║██╔════╝██╔═══██╗██║   ██║██╔════╝██╔════╝
+/// ██████╔╝██║██████╔╝██║     ██║██║   ██║   ██║   ███████║█████╗  ██║   ██║██║   ██║█████╗  ███████╗
+/// ██╔══██╗██║██╔══██╗██║     ██║██║   ██║   ██║   ██╔══██║██╔══╝  ██║▄▄ ██║██║   ██║██╔══╝  ╚════██║
+/// ██████╔╝██║██████╔╝███████╗██║╚██████╔╝   ██║   ██║  ██║███████╗╚██████╔╝╚██████╔╝███████╗███████║
+/// ╚═════╝ ╚═╝╚═════╝ ╚══════╝╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚══════╝ ╚══▀▀═╝  ╚═════╝ ╚══════╝╚══════╝
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <allegro.h> //Allegro pour l'affichage des graphes
+#include <time.h>
+#include <math.h>
 
-// structure a changer
-typedef struct {
-    float temps;
-} Arc;
+
+
+/// ███████╗████████╗██████╗ ██╗   ██╗ ██████╗████████╗██╗   ██╗██████╗ ███████╗███████╗
+/// ██╔════╝╚══██╔══╝██╔══██╗██║   ██║██╔════╝╚══██╔══╝██║   ██║██╔══██╗██╔════╝██╔════╝
+/// ███████╗   ██║   ██████╔╝██║   ██║██║        ██║   ██║   ██║██████╔╝█████╗  ███████╗
+/// ╚════██║   ██║   ██╔══██╗██║   ██║██║        ██║   ██║   ██║██╔══██╗██╔══╝  ╚════██║
+/// ███████║   ██║   ██║  ██║╚██████╔╝╚██████╗   ██║   ╚██████╔╝██║  ██║███████╗███████║
+/// ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝  ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝
+
+/* Structure d'un arc*/
+struct Arc {
+    int sommet; // numéro de sommet d'un arc adjacent au sommet initial
+    int valeur; //Poids de l'arc
+    struct Arc* arc_suivant;
+};
+typedef struct Arc* pArc; /* Alias de pointeur sur un Arc */
+
+/* Structure d'un sommet*/
+struct Sommet {
+    struct Arc* arc;
+    int *exclu; ///Stocke les id des sommets exclus
+    int visite;
+    int id;     //ID du sommet
+};
+typedef struct Sommet* pSommet; /* Alias de pointeur sur un Sommet */
+
+/* Structure d'un graphe*/
+typedef struct Graphe {
+    int ordre;  //Nombre de sommets
+    int taille; //Nombre d'arêtes / arcs
+    pSommet* pSommet;
+} Graphe; /* Alias d'un Graphe */
+
+
+typedef struct Exclu { //Tableau de structure qui pointe vers tous les sommets exclus
+    int id;
+    struct Exclu *suiv; //Pointeur vers le prochain élément du tableau
+} Exclude;
+
+
+/// ██████╗ ██████╗  ██████╗ ████████╗ ██████╗ ████████╗██╗   ██╗██████╗ ███████╗███████╗
+/// ██╔══██╗██╔══██╗██╔═══██╗╚══██╔══╝██╔═══██╗╚══██╔══╝╚██╗ ██╔╝██╔══██╗██╔════╝██╔════╝
+/// ██████╔╝██████╔╝██║   ██║   ██║   ██║   ██║   ██║    ╚████╔╝ ██████╔╝█████╗  ███████╗
+/// ██╔═══╝ ██╔══██╗██║   ██║   ██║   ██║   ██║   ██║     ╚██╔╝  ██╔═══╝ ██╔══╝  ╚════██║
+/// ██║     ██║  ██║╚██████╔╝   ██║   ╚██████╔╝   ██║      ██║   ██║     ███████╗███████║
+/// ╚═╝     ╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝    ╚═╝      ╚═╝   ╚═╝     ╚══════╝╚══════╝
+
+
+
 
 #endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_6_97_GIT_HEADER_H
